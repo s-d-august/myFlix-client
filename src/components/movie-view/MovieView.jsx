2179a61095280c67942425fb4e0897e32b627a67
s-d-august <sinclairaugust@gmail.com>
2024-06-08 13:22:37 -0500

    added components (doesn't work)

---
+++ src/components/movie-view/MovieView.jsx
@@ -0,0 +1,30 @@
+export const MovieView = ({movie, onBackClick}) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.ImagePath}/>
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Description:</span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
+      </div>
+      <div>
+        <span>Featured: </span>
+        <span>{movie.Featured}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  )
+}
