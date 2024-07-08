684e803e09628d29013b51718dcc82a81c6f6deb
s-d-august <sinclairaugust@gmail.com>
2024-07-08 13:24:27 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,27 @@
-import PropTypes from "prop-types"
-import {Button, Card} from "react-bootstrap"
-import { Link } from "react-router-dom"
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({movie}) => {
+export const MovieCard = ({ movie }) => {
   return (
-      <Card 
-      className="h-100" onClick={() => onMovieClick(movie)}>
-        <Card.Img variant="top" src={movie.Image} />
-        <Card.Body style={{backgroundColor: "white"}}>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Director}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+    <Card>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.author}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
           <Button variant="link">Open</Button>
         </Link>
-        </Card.Body>
-      </Card>)
-    }
-  
-
-
+      </Card.Body>
+    </Card>
+  );
+};
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
-    image: PropTypes.string
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    author: PropTypes.string
   }).isRequired
-}
+};
